c32b6d944912e19a1eac1872e13a24752d803062
yuqian-cao-19 <caoyuqian520@gmail.com>
2023-10-30 10:10:53 -0500

    Organized code under Cashier folder

--- project3-react-app/src/App.tsx
+++ project3-react-app/src/App.tsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Navigationbar from "./components/navbar.tsx";
-import Drinks from "./components/drinks.tsx";
-import Cart from "./components/cart.tsx";
+import Cashier from "./Cashier/Cashier.tsx";
 
 function App() {
   return (
     <div
       style={{ backgroundColor: "#CCE3DE", width: "100vw", height: "100vh" }}
     >
-      <Navigationbar></Navigationbar>
-      <Cart></Cart>
-      <Drinks></Drinks>
+      <Cashier></Cashier>
     </div>
   );
 }
