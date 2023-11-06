7537cc80ff0ce0937307c282ca820b3dba0a11bb
sarahb28 <sbrasseaux1@gmail.com>
2023-11-05 18:39:33 -0600

    frontend for employee mostly finished

--- frontend/src/components/employeePopup/employeePopup.tsx
+++ frontend/src/components/employeePopup/employeePopup.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import "./employeePopup.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle, faTimes } from "@fortawesome/free-solid-svg-icons";
+import IconPopup from "./iconPopup"; // Import the IconPopup component
 
 interface EmployeeData {
   name: string;
@@ -18,6 +22,7 @@ const EmployeePopup: React.FC<Props> = ({ isOpen, onClose, onSubmit }) => {
   const [position, setPosition] = useState('Employee');
   const [hoursPerWeek, setHoursPerWeek] = useState("");
   const [hourlyPay, setHourlyPay] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
 
   const handleSubmit = () => {
     const data: EmployeeData = {
@@ -26,8 +31,15 @@ const EmployeePopup: React.FC<Props> = ({ isOpen, onClose, onSubmit }) => {
       hoursPerWeek: parseInt(hoursPerWeek),
       hourlyPay: parseFloat(hourlyPay),
     };
-    onSubmit(data);
-    onClose();
+
+    // If not confirmed, mark it as confirmed
+    if (!confirmed) {
+      setConfirmed(true);
+    } else {
+      // If already confirmed, submit the data and close the popup
+      onSubmit(data);
+      onClose();
+    }
   };
 
   if (!isOpen) {
@@ -35,33 +47,52 @@ const EmployeePopup: React.FC<Props> = ({ isOpen, onClose, onSubmit }) => {
   }
 
   return (
+    <div className="popup-overlay">
     <div className={`popup ${isOpen ? "open" : ""}`}>
-      <div className="popup-content">
-        <h2>Add Employee</h2>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label>
-          Position:
-          <select value={position} onChange={(e) => setPosition(e.target.value)}>
-            <option value="Manager">Manager</option>
-            <option value="Employee">Employee</option>
-          </select>
-        </label>
-        <label>
-          Hours per Week:
-          <input type="number" value={hoursPerWeek} onChange={(e) => setHoursPerWeek(e.target.value)} />
-        </label>
-        <label>
-          Hourly Pay:
-          <input type="number" value={hourlyPay} onChange={(e) => setHourlyPay(e.target.value)} />
-        </label>
-        <div className="popup-buttons">
-          <button onClick={onClose}>Cancel</button>
-          <button onClick={handleSubmit}>Submit</button>
-        </div>
+      <div className="popup-header">
+        {/* Close button (x icon) in the top right */}
+        <button className="close-button" onClick={onClose}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
       </div>
+      <div className="popup-content">
+        {confirmed ? (
+          // Show confirmation icon if confirmed
+          <IconPopup isOpen={confirmed} onClose={onClose} icon={faCheckCircle} color="green" />
+        ) : (
+          // Show input fields and buttons if not confirmed
+          <>
+            <label>
+              Name:
+              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              Position:
+              <select value={position} onChange={(e) => setPosition(e.target.value)}>
+                <option value="Manager">Manager</option>
+                <option value="Employee">Employee</option>
+              </select>
+            </label>
+            <label>
+              Hours per Week:
+              <input type="number" value={hoursPerWeek} onChange={(e) => setHoursPerWeek(e.target.value)} />
+            </label>
+            <label>
+              Hourly Pay:
+              <input type="number" value={hourlyPay} onChange={(e) => setHourlyPay(e.target.value)} />
+            </label>
+            <div className="popup-buttons">
+              <button onClick={onClose}>
+                Remove
+              </button>
+              <button onClick={handleSubmit} disabled={confirmed}>
+                Confirm
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
     </div>
   );
 };
