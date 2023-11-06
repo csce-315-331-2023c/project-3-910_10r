f5c16774d5cd494102da8dee4a380d1e1f4cd085
sarahb28 <sbrasseaux1@gmail.com>
2023-11-05 19:19:08 -0600

    fix link to Cashier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-//import Cashier from "./pages/Cashier/cashier.tsx";
+import Cashier from "./pages/Cashier/cashier.tsx";
 import Payment from "./pages/Cashier/Payment.tsx";
 // import axios from "axios";
 // import React, { useEffect } from "react";
