fafae55cc7d433de24a78d4f17800782ec3680d0
sarahb28 <sbrasseaux1@gmail.com>
2023-11-21 14:13:00 -0600

    finished reports page

--- backend/reports/queries.js
+++ backend/reports/queries.js
@@ -1,45 +1,66 @@
-const excessReport = "WITH current_inventory AS ( \
-    SELECT name, amount \
-    FROM inventory \
-), \
-sales_by_item AS ( \
-    SELECT \
-        i.name AS item_name, \
-        COUNT(o.drink_id) AS sold_count \
-    FROM orders o \
-    JOIN recipes r ON o.drink_id = r.recipeid \
-    JOIN inventory i ON r.ingredient_names = i.name \
-    WHERE o.date >= $1::date AND o.date <= $2::date \
-    GROUP BY i.name \
-), \
-total_inventory AS ( \
-    SELECT \
-        ci.name AS item_name, \
-        ci.amount AS current_amount, \
-        COALESCE(sb.sold_count, 0) AS sold_count \
-    FROM current_inventory ci \
-    LEFT JOIN sales_by_item sb ON ci.name = sb.item_name \
-), \
-excess_items AS ( \
-    SELECT item_name \
-    FROM total_inventory \
-    WHERE sold_count < (current_amount + sold_count) * 0.1 \
-) \
-SELECT item_name \
-FROM excess_items \
-UNION ALL \
-SELECT 'cups' AS item_name \
-WHERE (SELECT COUNT(o.drink_id) FROM orders WHERE date >= $1::date AND date <= $2::date) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'cups') * 0.1 \
-UNION ALL \
-SELECT 'straws' AS item_name \
-WHERE (SELECT COUNT(o.drink_id) FROM orders WHERE date >= $1::date AND date <= $2::date) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'straws') * 0.1 \
-UNION ALL \
-SELECT 'plastic cover' AS item_name \
-WHERE (SELECT COUNT(o.drink_id) FROM orders WHERE date >= $1::date AND date <= $2::date) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'plastic cover') * 0.1 \
-UNION ALL \
-SELECT 'napkins' AS item_name \
-WHERE (SELECT COUNT(o.drink_id) FROM orders WHERE date >= $1::date AND date <= $2::date) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'napkins');"
-;
+const excessReport = `
+WITH current_inventory AS (
+    SELECT name, amount
+    FROM inventory
+), 
+sales_by_item AS (
+    SELECT
+        i.name AS item_name,
+        COUNT(r.recipeid) AS sold_count
+    FROM orders o
+    JOIN recipes r ON r.recipeid = ANY(o.drink_id)
+    JOIN inventory i ON i.name = ANY(r.ingredient_names)
+    WHERE o.date >= $1::date AND o.date <= $2::date
+    GROUP BY i.name
+), 
+total_inventory AS (
+    SELECT
+        ci.name AS item_name,
+        ci.amount AS current_amount,
+        COALESCE(sb.sold_count, 0) AS sold_count
+    FROM current_inventory ci
+    LEFT JOIN sales_by_item sb ON ci.name = sb.item_name
+), 
+excess_items AS (
+    SELECT item_name
+    FROM total_inventory
+    WHERE sold_count < (current_amount + sold_count) * 0.1
+)
+SELECT item_name
+FROM excess_items
+UNION ALL
+SELECT 'cups' AS item_name
+WHERE (
+    SELECT COUNT(r.recipeid) 
+    FROM orders o 
+    JOIN recipes r ON r.recipeid = ANY(o.drink_id) 
+    WHERE o.date >= $1::date AND o.date <= $2::date
+) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'cups') * 0.1
+UNION ALL
+SELECT 'straws' AS item_name
+WHERE (
+    SELECT COUNT(r.recipeid) 
+    FROM orders o 
+    JOIN recipes r ON r.recipeid = ANY(o.drink_id) 
+    WHERE o.date >= $1::date AND o.date <= $2::date
+) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'straws') * 0.1
+UNION ALL
+SELECT 'plastic cover' AS item_name
+WHERE (
+    SELECT COUNT(r.recipeid) 
+    FROM orders o 
+    JOIN recipes r ON r.recipeid = ANY(o.drink_id) 
+    WHERE o.date >= $1::date AND o.date <= $2::date
+) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'plastic cover') * 0.1
+UNION ALL
+SELECT 'napkins' AS item_name
+WHERE (
+    SELECT COUNT(r.recipeid) 
+    FROM orders o 
+    JOIN recipes r ON r.recipeid = ANY(o.drink_id) 
+    WHERE o.date >= $1::date AND o.date <= $2::date
+) * 0.9 < (SELECT current_amount FROM current_inventory WHERE name = 'napkins') * 0.1;
+`;
 
 module.exports = {
     excessReport,
--- frontend/src/pages/Manager/Reports/reports.tsx
+++ frontend/src/pages/Manager/Reports/reports.tsx
@@ -5,60 +5,395 @@
 //when already on Custom Time, if you do the dropdown and click it again, the popup is not popping up
 
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { registerLocale } from "react-datepicker";
 import enGB from "date-fns/locale/en-GB"; // Import a locale for date-fns
 import "./reports.scss";
 import CustomTimePopup from "../../../components/reportsPopup/customTime";
 
+import axios, { AxiosInstance } from 'axios';
+
+let baseURL = import.meta.env.VITE_API_URL;
+
+const API: AxiosInstance = axios.create({
+  baseURL: baseURL,
+  timeout: 10000
+});
+
 // Register the locale to use with react-datepicker
 registerLocale("en-GB", enGB);
 
-const commonlySoldData = [
-  { firstString: "Product A", secondString: "Product B", count: 250 },
-  { firstString: "Product C", secondString: "Product D", count: 400 },
-  { firstString: "Product E", secondString: "Product F", count: 340 },
-  { firstString: "Product A", secondString: "Product B", count: 250 },
-  { firstString: "Product C", secondString: "Product D", count: 400 },
-  { firstString: "Product E", secondString: "Product F", count: 340 },
-  { firstString: "Product A", secondString: "Product B", count: 250 },
-  { firstString: "Product C", secondString: "Product D", count: 400 },
-  { firstString: "Product E", secondString: "Product F", count: 340 },
-];
-
-const excessIngredientsData = [
-  {ingredientName: "Black tea"},{ingredientName: "Mango"},{ingredientName: "water"}, 
-  {ingredientName: "Winter Melon"}, {ingredientName: "Green Tea"},
-];
-
 const Reports = () => {
   const [selectedOption, setSelectedOption] = useState("last24");
-  const [startDate, setStartDate] = useState<Date | null>(null); // Specify the type
-  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [startDate, setStartDate] = useState<string | null>(null); // Specify the type
+  const [endDate, setEndDate] = useState<string | null>(null);
   const [showCustomTimePopup, setShowCustomTimePopup] = useState(false);
+  const [pairCount, setPairCount] = useState<{ [key: string]: number }>({});
+  const [excessIngredients, setExcessIngredients] = useState<string[]>([]);
+  const [ingredientsMap, setIngredientsMap] = useState<{[key: string]: number}>({});
+  const [inventoryInfo, setInventoryInfo] = useState<{[key: string]: number}>({});
+  const [loadingExcess, setLoadingExcess] = useState(false);
+
+  useEffect(() => {
+    fetch24HoursData(); // Fetch data for last 24 hours on component mount
+  }, []);
+
+  /*const clearData = () => {
+    setExcessIngredients([]);
+    setIngredientsMap({});
+    setInventoryInfo({});
+  }*/
+
+  const fetch24HoursData = () => {
+    const currentDate = new Date();
+    const beginningDate = new Date(currentDate.getTime() - 24 * 60 * 60 * 1000); // 24 hours ago
+    const endDate = currentDate;
+    const formattedBeginningDate = formatDate(beginningDate);
+    const formattedEndDate = formatDate(endDate);
+
+    if (formattedBeginningDate && formattedEndDate) {
+      setLoadingExcess(true);
+      fetchData(formattedBeginningDate, formattedEndDate);
+      //clearData();
+      fetchExcessData(formattedBeginningDate, formattedEndDate);
+      //calculateTotals();
+    } else {
+      console.error("Formatted beginning date is null");
+    }
+  };
+
+  const fetch7DaysData = () => {
+    const currentDate = new Date();
+    const beginningDate = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days ago
+    const endDate = currentDate;
+    const formattedBeginningDate = formatDate(beginningDate);
+    const formattedEndDate = formatDate(endDate);
+
+    if (formattedBeginningDate && formattedEndDate) {
+      setLoadingExcess(true);
+      fetchData(formattedBeginningDate, formattedEndDate);
+      //clearData();
+      fetchExcessData(formattedBeginningDate, formattedEndDate);
+      //calculateTotals();
+    } else {
+      console.error("Formatted beginning date is null");
+    }
+  };
+  const fetch30DaysData = () => {
+    const currentDate = new Date();
+    const beginningDate = new Date(currentDate.getTime() - 30 * 24 * 60 * 60 * 1000); // 30 days ago
+    const endDate = currentDate;
+    const formattedBeginningDate = formatDate(beginningDate);
+    const formattedEndDate = formatDate(endDate);
+
+    if (formattedBeginningDate && formattedEndDate) {
+      setLoadingExcess(true);
+      fetchData(formattedBeginningDate, formattedEndDate);
+      //clearData();
+      fetchExcessData(formattedBeginningDate, formattedEndDate);
+      //calculateTotals();
+    } else {
+      console.error("Formatted beginning date is null");
+    }
+  };
+
+  const fetch180DaysData = () => {
+    const currentDate = new Date();
+    const beginningDate = new Date(currentDate.getTime() - 180 * 24 * 60 * 60 * 1000); // 180 days ago
+    const endDate = currentDate;
+    const formattedBeginningDate = formatDate(beginningDate);
+    const formattedEndDate = formatDate(endDate);
+
+    if (formattedBeginningDate && formattedEndDate) {
+      setLoadingExcess(true);
+      fetchData(formattedBeginningDate, formattedEndDate);
+      //clearData();
+      fetchExcessData(formattedBeginningDate, formattedEndDate);
+      //calculateTotals();
+    } else {
+      console.error("Formatted beginning date is null");
+    }
+  };
+
+  const fetch365DaysData = () => {
+    const currentDate = new Date();
+    const beginningDate = new Date(currentDate.getTime() - 365 * 24 * 60 * 60 * 1000); // 365 days ago
+    const endDate = currentDate;
+    const formattedBeginningDate = formatDate(beginningDate);
+    const formattedEndDate = formatDate(endDate);
+
+    if (formattedBeginningDate && formattedEndDate) {
+      setLoadingExcess(true);
+      fetchData(formattedBeginningDate, formattedEndDate);
+      //clearData();
+      fetchExcessData(formattedBeginningDate, formattedEndDate);
+      //calculateTotals();
+    } else {
+      console.error("Formatted beginning date is null");
+    }
+  };
 
   const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = e.target.value;
-  
-    // Check if "Custom time" is selected and display CustomTimePopup
+
     if (selectedValue === "custom") {
       setShowCustomTimePopup(true);
     } else {
       setShowCustomTimePopup(false);
+      setSelectedOption(selectedValue);
+
+      // Check if both dates are selected and make API call
+      if (startDate && endDate) {
+        setLoadingExcess(true);
+        fetchData(startDate, endDate);
+        //clearData();
+        fetchExcessData(startDate,endDate);
+        //calculateTotals();
+      }
     }
-  
-    // Update the selected option
-    setSelectedOption(selectedValue);
+
+    if (selectedValue === "last24") {
+      fetch24HoursData();
+    } else if (selectedValue === "last7") {
+      fetch7DaysData();
+    } else if (selectedValue === "last30") {
+      fetch30DaysData();
+    } else if (selectedValue === "last180") {
+      fetch180DaysData();
+    } else if (selectedValue === "last365") {
+      fetch365DaysData();
+    }
   };
 
-  const handleCustomTimeSubmit = () => {
-    // Handle custom time selection
-    if (startDate && endDate) {
-      console.log("Selected Start Date:", startDate.toLocaleDateString());
-      console.log("Selected End Date:", endDate.toLocaleDateString());
+  const formatDate = (date: Date | null): string | null => {
+    if (date) {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
     }
-    setShowCustomTimePopup(false);
+    return null;
   };
+
+  const fetchExcessData = async (beginningDate: string, endDate: string) => {
+    try{
+      const inventoryInfo2: { [key: string]: number } = {};
+      const drinkCountMap: { [key: string]: number } = {};
+      const ingredientsMap2: { [key: string]: number } = {};
+    //setLoadingExcess(true);
+    // Fetch drink count data
+    setIngredientsMap({});
+    setInventoryInfo({});
+    const response = await API.get("/reports/getExcessReport/orderData", {
+      params: { beginningDate, endDate }
+    });
+    const data = response.data;
+
+  
+    if (data && Array.isArray(data) && data.length > 0) {
+      data.forEach((item: any) => {
+        const drinkIds = item.drink_id;
+
+        if (Array.isArray(drinkIds) && drinkIds.length > 0) {
+          drinkIds.forEach((drinkId: string) => {
+            if (drinkId !== "0"){
+            drinkCountMap[drinkId] = (drinkCountMap[drinkId] || 0) + 1;
+            }
+            else{
+              console.log("processing: ", drinkId);
+            }
+          });
+        } else {
+          console.error('Drink IDs are not in the expected format or length');
+        }
+      });
+    }
+
+    //console.log(drinkCountMap);
+    delete drinkCountMap[0];
+
+
+
+    // Fetch inventory information
+    const inventoryResponse = await API.get("/reports/getExcessReport/inventoryInfo",{});
+        const invdata = inventoryResponse.data;
+  
+      if (invdata){
+        invdata.forEach((item: any) => {
+          const name = item.name;
+          const amount = parseFloat(item.amount);
+
+          //console.log('Ingredient Name:', name); // Log each ingredient name
+          //console.log('Ingredient Value:', amount);
+  
+          inventoryInfo2[name] = amount;
+          //console.log(inventoryInfo[name]);
+        });
+      } else{
+        console.error("No inventory data");
+      }  
+      
+  
+      // Calculate multiplied amounts for each ingredient
+      for (const drinkId in drinkCountMap) {
+        if (drinkCountMap.hasOwnProperty(drinkId)) {
+
+          const count = drinkCountMap[drinkId];
+
+          //add straws, cups, napkins and plastic cover
+          ingredientsMap2["straws"] = (ingredientsMap2["straws"] || 0) + count;
+          ingredientsMap2["cups"] = (ingredientsMap2["cups"] || 0) + count;
+          ingredientsMap2["napkins"] = (ingredientsMap2["napkins"] || 0) + count;
+          ingredientsMap2["plastic cover"] = (ingredientsMap2["plastic cover"] || 0) + count;
+          //console.log(count);
+
+          // Fetch recipe data for each drinkId
+          const recipeResponse = await API.get("/reports/getExcessReport/recipeData", {
+            params: { drink: drinkId }
+          });
+              const recipedata = recipeResponse.data;
+              //console.log(data);
+
+              if (recipedata && Array.isArray(recipedata) && recipedata.length > 0) {
+                recipedata.forEach((item: any) => {
+                  const ingredientNames = item.ingredientNames;
+                  const ingredientValues = item.ingredientValues;
+
+                  if (ingredientNames && ingredientValues && Array.isArray(ingredientNames) && Array.isArray(ingredientValues)) {
+                    for (let i = 0; i < ingredientNames.length; i++) {
+                      const ingredientName = ingredientNames[i];
+                      const ingredientValue = ingredientValues[i];
+
+                      if (typeof ingredientValue === 'number') {
+                        ingredientsMap2[ingredientName] = (ingredientsMap2[ingredientName] || 0) + (ingredientValue * count);
+                        //console.log(ingredientName, count, ingredientValue);
+                        //console.log(ingredientsMap2["black tea"]);
+                      } else {
+                        console.error(`Invalid value for ${ingredientName}`);
+                      }
+                      // console.log(ingredientsMap[ingredientName]);
+                    }
+                  }
+                });
+              }
+
+                //console.log(ingredientsMap);
+              } else {
+                console.error('No data received or data format issue');
+              }
+                //setLoadingExcess(false);
+        }
+
+      if (Object.keys(ingredientsMap2).length > 0 && Object.keys(inventoryInfo2).length > 0){
+          //console.log(ingredientsMap);
+          setInventoryInfo(inventoryInfo2);
+          setIngredientsMap(ingredientsMap2);
+          //await calculateTotals();
+      }
+      //setLoadingExcess(false);
+      }
+      catch(error) {
+        console.error(error);
+        //setLoadingExcess(false);
+      }
+  };
+
+  useEffect(() => {
+    calculateTotals();
+    setLoadingExcess(false);
+  }, [ingredientsMap, inventoryInfo]);
+
+  /*useEffect(() => {
+    // Calculate totals when ingredientsMap or inventoryInfo are updated
+    calculateTotals();
+  }, [ingredientsMap, inventoryInfo]); */
+
+  const calculateTotals = () => {
+    setExcessIngredients([]);
+    const excess = [];
+    // Compare ingredientsMap with inventoryInfo
+    console.log(ingredientsMap);
+    console.log(inventoryInfo);
+    for (const ingredientName in ingredientsMap) {
+      //console.log(inventoryInfo[ingredientName]);
+        const ingredientValue = ingredientsMap[ingredientName];
+        const inventoryAmount = inventoryInfo[ingredientName];
+        //console.log(ingredientValue, inventoryAmount);
+        if (inventoryAmount && ingredientValue < (ingredientValue + inventoryAmount) * 0.1) {
+          excess.push(ingredientName);
+          //console.log("true");
+        }
+    }
+    console.log("excess: " , excess);
+    setExcessIngredients(excess);
+    //setLoadingExcess(false);
+  }
+
+
+  const fetchData = (beginningDate: string, endDate: string) => {
+    API.get("/managers/whatSalesTogether", {
+      params: { beginningDate, endDate }
+    })
+      .then((response) => {
+        const data = response.data;
+  
+        if (data && Array.isArray(data) && data.length > 0) {
+          const drinkCountMap: { [key: string]: number } = {};
+  
+          data.forEach((item: any) => {
+            const drinkIds = item.drink_id;
+  
+            if (Array.isArray(drinkIds) && drinkIds.length > 1) {
+              const sortedIds = drinkIds.sort(); // Sort the IDs to ensure consistent combinations
+  
+              for (let i = 0; i < sortedIds.length; i++) {
+                for (let j = i + 1; j < sortedIds.length; j++) {
+                  if (sortedIds[i] !== sortedIds[j]) { // Exclude pairs with the same ID
+                    const pair = `${sortedIds[i]}-${sortedIds[j]}`;
+                    drinkCountMap[pair] = (drinkCountMap[pair] || 0) + 1;
+                  }
+                }
+              }
+            } else {
+              console.error('Drink IDs are not in the expected format or length');
+            }
+          });
+  
+          // Convert drinkCountMap to an array of key-value pairs and sort by count
+          const sortedDrinkCountArray = Object.entries(drinkCountMap)
+            .sort((a, b) => b[1] - a[1]);
+  
+          // Convert the sorted array back to an object
+          const sortedDrinkCountMap: { [key: string]: number } = {};
+          sortedDrinkCountArray.forEach(([pair, count]) => {
+            sortedDrinkCountMap[pair] = count;
+          });
+
+          setPairCount(sortedDrinkCountMap);
+        } else {
+          console.error('Data is empty or not in the expected format');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  
+  
+
+  const handleCustomTimeSubmit = () => {
+    if (startDate && endDate) {
+      setStartDate(formatDate(new Date(startDate)));
+      setEndDate(formatDate(new Date(endDate)));
+      console.log("Selected Start Date:", startDate);
+      console.log("Selected End Date:", endDate);
+      setShowCustomTimePopup(false);
+      setSelectedOption("custom");
+      fetchData(startDate, endDate);
+      fetchExcessData(startDate, endDate);
+    }
+  };
+
   return (
     <div className="main-container">
       <div className="header">
@@ -73,7 +408,7 @@ const Reports = () => {
           </select>
           <div className="custom-date-display">
             {selectedOption === "custom" && startDate && endDate && (
-              `From: ${startDate.toLocaleDateString()}   To: ${endDate.toLocaleDateString()}`
+              `From: ${startDate}   To: ${endDate}`
             )}
           </div>
         </div>
@@ -83,12 +418,12 @@ const Reports = () => {
         <div className="column">
           <h2>Commonly Sold Together</h2>
           <div className="scrollable-content">
-            {commonlySoldData.map((data, index) => (
+            {Object.entries(pairCount).map(([pair, count], index) => (
               <div className="data-box" key={index}>
-                <div className="number">{data.count}</div>
-                <div className="data-entry">{data.firstString}</div>
+                <div className="number">{count}</div>
+                <div className="data-entry">{pair.split('-')[0]}</div>
                 <div className="separator">|</div>
-                <div className="data-entry">{data.secondString}</div>
+                <div className="data-entry">{pair.split('-')[1]}</div>
               </div>
             ))}
           </div>
@@ -96,11 +431,21 @@ const Reports = () => {
         <div className="column">
           <h2>Ingredients in Excess</h2>
           <div className="scrollable-content">
-            {excessIngredientsData.map((data, index) => (
-              <div className="data-box" key={index}>
-                <div className="data-entry">{data.ingredientName}</div>
+            {excessIngredients.length === 0 && loadingExcess ? (
+              <div className="data-box">
+                <div className="data-entry">Loading...</div>
               </div>
-            ))}
+            ) : excessIngredients.length === 0 ? (
+              <div className="data-box">
+                <div className="data-entry">None</div>
+              </div>
+            ) : (
+              excessIngredients.map((data, index) => (
+                <div className="data-box" key={index}>
+                  <div className="data-entry">{data}</div>
+                </div>
+              ))
+            )}
           </div>
         </div>
       </div>
@@ -108,10 +453,10 @@ const Reports = () => {
       {showCustomTimePopup && (
         <CustomTimePopup
           isOpen={showCustomTimePopup}
-          startDate={startDate}
-          endDate={endDate}
-          onStartDateChange={setStartDate}
-          onEndDateChange={setEndDate}
+          startDate={startDate ? new Date(startDate) : null}
+          endDate={endDate ? new Date(endDate) : null}
+          onStartDateChange={(date: Date | null) => setStartDate(formatDate(date))}
+          onEndDateChange={(date: Date | null) => setEndDate(formatDate(date))}
           onClose={() => setShowCustomTimePopup(false)}
           onSubmit={handleCustomTimeSubmit}
         />
