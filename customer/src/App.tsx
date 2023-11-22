be78a6bdd90abfd7daaaff85502f49da4df5f931
eunsooyeo <eunsooyeo@tamu.edu>
2023-11-22 11:09:18 -0600

    Resolve render issues

--- customer/src/App.tsx
+++ customer/src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import Cashier from "./pages/Cashier/cashier.tsx";
-import IntroPage from "./pages/Intro/landing.tsx";
+// import Cashier from "./pages/Cashier/cashier.tsx";
+// import IntroPage from "./pages/Intro/landing.tsx";
 import MenuBoard from "./pages/MenuBoard/menuBoard.tsx";
 
 function App() {
   const [/* isLanding */, setIsLanding] = useState<boolean>(false);
-  const [isOrder, setIsOrder] = useState<boolean>(false);
-  const [isMenu, setIsMenu] = useState<boolean>(false);
-  const [isHome, setIsHome] = useState<boolean>(true);
+  // const [isOrder, setIsOrder] = useState<boolean>(false);
+  // const [isMenu, setIsMenu] = useState<boolean>(false);
+  // const [isHome, setIsHome] = useState<boolean>(true);
   
   return (
     <>
