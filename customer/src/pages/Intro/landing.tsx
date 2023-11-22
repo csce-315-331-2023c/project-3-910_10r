d182483aea8d9bbf64748e115284e88249ba8180
Dicong Wang <dicong.wang02@gmail.com>
2023-11-22 13:07:10 -0600

    connected menuBoard and front page for customer

--- customer/src/pages/Intro/landing.tsx
+++ customer/src/pages/Intro/landing.tsx
@@ -4,12 +4,13 @@ import CustomerHeader from "../../components/header/header.tsx";
 import { Carousel } from '../../components/carousel/Carousel.tsx';
 import {slides} from "../../assets/carouselData.json";
 import OrderButton from './orderButton.tsx';
-
+import MenuBoardButton from './menuBoardButton.tsx';
 interface IntroPageProps {
   isReadyToOrder: React.Dispatch<React.SetStateAction<boolean>>;
+  isLookingAtMenu: React.Dispatch<React.SetStateAction<boolean>>;
   setWhichPage : React.Dispatch<React.SetStateAction<boolean>>;
 }
-function IntroPage({isReadyToOrder, setWhichPage} : IntroPageProps) {
+function IntroPage({isReadyToOrder, isLookingAtMenu, setWhichPage} : IntroPageProps) {
   return (
     <div className="intro-page">
         <CustomerHeader setWhichPage={setWhichPage}></CustomerHeader>
@@ -21,6 +22,7 @@ function IntroPage({isReadyToOrder, setWhichPage} : IntroPageProps) {
           {/* Your buttons or additional content can go here */}
         </div>
         <div>
+            <MenuBoardButton setMenuPage = {isLookingAtMenu}></MenuBoardButton>
             <Carousel data = {{slides}}/>
             <p className='orderParagraph'>Ready to Order?</p>
             <OrderButton  setOrderPage={isReadyToOrder}></OrderButton>
